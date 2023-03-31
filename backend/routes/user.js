d1c8a89afc215fe0acb6a4e904296fe226027975
Zacharie Dalloz <zachariedalloz@icloud.com>
2023-03-31 17:13:57 +0200

    1 test passed user (error 404)

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,123 @@
 import express from 'express'
 import { User, Product, Bid } from '../orm/index.js'
+import { sequelize } from '../orm/database.js'
 
 const router = express.Router()
 
-router.get('/api/users/:userId', async (req, res) => {
-  res.status(600).send()
+router.get('/api/users/:userId/products', async (req, res) => {
+  try {
+    const userId = req.params.userId
+    const products = await Product.findAll({
+      where: {
+        sellerId: userId
+      },
+      include: [
+        {
+          model: Bid,
+          attributes: [
+            'amount',
+            'createdAt',
+            'status',
+            [sequelize.fn('max', sequelize.col('amount')), 'highestBid']
+          ],
+          group: ['productId'],
+          raw: true
+        }
+      ]
+    })
+    const productList = products.map(product => ({
+      name: product.name,
+      description: product.description,
+      photo: product.photo,
+      currentPrice: product.bids[0].highestBid || product.startingPrice,
+      bidCount: product.bids.length,
+      link: `/product/${product.id}`
+    }))
+    res.json(productList)
+  } catch (err) {
+    console.error(err)
+    res.status(500).send('Server error')
+  }
+})
+
+router.get('/api/users/:userId/bids', async (req, res) => {
+  try {
+    const userId = req.params.userId
+    const bids = await Bid.findAll({
+      where: {
+        userId
+      },
+      include: [Product]
+    })
+    const bidList = bids.map(bid => ({
+      name: bid.product.name,
+      link: `/product/${bid.productId}`,
+      highestBid: bid.amount,
+      date: bid.createdAt,
+      status: bid.status
+    }))
+    res.json(bidList)
+  } catch (err) {
+    console.error(err)
+    res.status(500).send('Server error')
+  }
+})
+
+// Get products and bids of authenticated user
+router.get('/users/me', async (req, res) => {
+  try {
+    if (!req.user) {
+      return res.redirect('/login')
+    }
+    const userId = req.user.id
+    const products = await Product.findAll({
+      where: {
+        sellerId: userId
+      },
+      include: [
+        {
+          model: Bid,
+          attributes: [
+            'amount',
+            'createdAt',
+            'status',
+            [sequelize.fn('max', sequelize.col('amount')), 'highestBid']
+          ],
+          group: ['productId'],
+          raw: true
+        }
+      ]
+    })
+    const productList = products.map(product => ({
+      name: product.name,
+      description: product.description,
+      photo: product.photo,
+      currentPrice: product.bids[0].highestBid || product.startingPrice,
+      bidCount: product.bids.length,
+      link: `/product/${product.id}`
+    }))
+    const bids = await Bid.findAll({
+      where: {
+        userId
+      },
+      include: [Product]
+    })
+    const bidList = bids.map(bid => ({
+      name: bid.product.name,
+      link: `/product/${bid.productId}`,
+      highestBid: bid.amount,
+      date: bid.createdAt,
+      status: bid.status
+    }))
+    res.render('user', {
+      user: req.user,
+      productList,
+      bidList
+    })
+  } catch (err) {
+    console.error(err)
+    res.status(500).send('Server error')
+  }
 })
 
 export default router
